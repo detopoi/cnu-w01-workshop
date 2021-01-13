aa3f826a1b6f691656b9fb82b24e4fe14ac52639
detopoi <907867549@qq.com>
2021-01-13 13:54:32 +0000

    hahaha

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-name = 'Josephine'
-age = 36
-
+name = 'Xiangting Long'
+age = 19
+birthday = "27th September"
 print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'My birthday is {birthday}')
